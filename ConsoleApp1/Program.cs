73732ac23071ac91d987c7102a774466f85dcc86
Andrey Odintsov <morrigun0@gmail.com>
2024-10-11 01:45:23 +0300

    without comments

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,138 +10,138 @@ namespace Arrays1
     {
         static void Main(string[] args)
         {
-            ////Task 1
-            //{
-            //    int[] arr1 = new int[10];
-            //    Random rnd = new Random();
-            //    for(int i = 0; i < arr1.Length; i++)
-            //    {
-            //        arr1[i] = rnd.Next(-20, 20);
-            //        Console.Write(arr1[i] + " ");
-            //    }
+            //Task 1
+            {
+                int[] arr1 = new int[10];
+                Random rnd = new Random();
+                for (int i = 0; i < arr1.Length; i++)
+                {
+                    arr1[i] = rnd.Next(-20, 20);
+                    Console.Write(arr1[i] + " ");
+                }
 
-            //    Console.WriteLine();
+                Console.WriteLine();
 
-            //    Console.WriteLine("Четные элементы: ");
+                Console.WriteLine("Четные элементы: ");
 
-            //    for (int i = 0;i<arr1.Length;i++)
-            //    {
-            //        if(i%2==0)
-            //            Console.Write(arr1[i] + " ");
-            //    }
+                for (int i = 0; i < arr1.Length; i++)
+                {
+                    if (i % 2 == 0)
+                        Console.Write(arr1[i] + " ");
+                }
 
-            //    Console.WriteLine();
+                Console.WriteLine();
 
-            //    Console.WriteLine("Нечетные элементы: ");
-
-
-            //    for (int i = 0; i < arr1.Length; i++)
-            //    {
-            //        if (i % 2 == 1)
-            //            Console.Write(arr1[i] + " ");
-            //    }
-
-            //    Console.WriteLine();
-
-            //    Array.Sort(arr1);
-
-            //    Console.WriteLine("Отсортированные элементы: ");
+                Console.WriteLine("Нечетные элементы: ");
 
 
-            //    foreach (int i in arr1)
-            //        Console.Write(i + " ");
+                for (int i = 0; i < arr1.Length; i++)
+                {
+                    if (i % 2 == 1)
+                        Console.Write(arr1[i] + " ");
+                }
 
-            //    Console.WriteLine();
+                Console.WriteLine();
 
-            //    Console.WriteLine("Уникальные элементы: ");
+                Array.Sort(arr1);
 
-            //    for (int i = 0; i < arr1.Length; i++)
-            //    {
-            //        if (i == arr1.Length - 1 || arr1[i] != arr1[i + 1])
-            //            Console.Write(arr1[i] + " ");
-
-            //        else if (arr1[i] == arr1[i + 1])
-            //            i++;
-            //    }
-
-            //    Console.WriteLine();
-            //    Console.WriteLine();
-            //    Console.WriteLine();
+                Console.WriteLine("Отсортированные элементы: ");
 
 
-            //}
+                foreach (int i in arr1)
+                    Console.Write(i + " ");
 
-            ////Task 2 
-            //{
-            //    int[] arr2 = new int[10];
-            //    Random rnd = new Random();
-            //    for (int i = 0;i< arr2.Length; i++)
-            //    {
-            //        arr2[i] = rnd.Next(-20, 20);
-            //        Console.Write(arr2[i] + " ");
-            //    }
-            //    Console.WriteLine();
+                Console.WriteLine();
 
-            //    Console.WriteLine("Введите параметр:");
-            //    int param = int.Parse(Console.ReadLine());
+                Console.WriteLine("Уникальные элементы: ");
 
-            //    Console.WriteLine("Значения строго меньше параметра: ");
+                for (int i = 0; i < arr1.Length; i++)
+                {
+                    if (i == arr1.Length - 1 || arr1[i] != arr1[i + 1])
+                        Console.Write(arr1[i] + " ");
 
-            //    for(int i = 0;i< arr2.Length; i++)
-            //    {
-            //        if (arr2[i] < param)
-            //        {
-            //            Console.Write(arr2[i] + " ");
-            //        }
-            //    }
+                    else if (arr1[i] == arr1[i + 1])
+                        i++;
+                }
 
-            //    Console.WriteLine();
-            //    Console.WriteLine();
-            //    Console.WriteLine();
-            //}
-
-            ////Task 3
-            //{
-            //    int[] arr3 = new int[] { 7, 6, 5, 3, 4, 7, 6, 5, 3, 7, 6, 5 };
-            //    Console.WriteLine("Введите последовательность из трех чисел:");
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine();
 
 
-            //    int[] tmpArr = new int[3];
-            //    for(int i = 0; i < tmpArr.Length; i++)
-            //        tmpArr[i] = int.Parse(Console.ReadLine());
+            }
 
-            //    int count = 0;
-            //    bool flag;
+            //Task 2 
+            {
+                int[] arr2 = new int[10];
+                Random rnd = new Random();
+                for (int i = 0; i < arr2.Length; i++)
+                {
+                    arr2[i] = rnd.Next(-20, 20);
+                    Console.Write(arr2[i] + " ");
+                }
+                Console.WriteLine();
 
-            //    for(int i =0;i<arr3.Length; i++)
-            //    {
-            //        flag = false;
-            //        for(int j =0;j<tmpArr.Length;j++)
-            //        {
-            //            if (tmpArr[j] == arr3[i] && i != arr3.Length-1)
-            //            {
-            //                i++;
+                Console.WriteLine("Введите параметр:");
+                int param = int.Parse(Console.ReadLine());
 
-            //                if (j == tmpArr.Length-1)
-            //                    flag = true;
-            //                continue;
-            //            }
-            //            else
-            //                break;
+                Console.WriteLine("Значения строго меньше параметра: ");
 
-            //        }
-            //        if (flag)
-            //            count++;
+                for (int i = 0; i < arr2.Length; i++)
+                {
+                    if (arr2[i] < param)
+                    {
+                        Console.Write(arr2[i] + " ");
+                    }
+                }
 
-            //    }
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine();
+            }
 
-            //    Console.WriteLine("Ваша последовательность встречается в массиве " + count + " раз");
+            //Task 3
+            {
+                int[] arr3 = new int[] { 7, 6, 5, 3, 4, 7, 6, 5, 3, 7, 6, 5 };
+                Console.WriteLine("Введите последовательность из трех чисел:");
 
-            //    Console.WriteLine();
-            //    Console.WriteLine();
-            //    Console.WriteLine();
 
-            //}
+                int[] tmpArr = new int[3];
+                for (int i = 0; i < tmpArr.Length; i++)
+                    tmpArr[i] = int.Parse(Console.ReadLine());
+
+                int count = 0;
+                bool flag;
+
+                for (int i = 0; i < arr3.Length; i++)
+                {
+                    flag = false;
+                    for (int j = 0; j < tmpArr.Length; j++)
+                    {
+                        if (tmpArr[j] == arr3[i] && i != arr3.Length - 1)
+                        {
+                            i++;
+
+                            if (j == tmpArr.Length - 1)
+                                flag = true;
+                            continue;
+                        }
+                        else
+                            break;
+
+                    }
+                    if (flag)
+                        count++;
+
+                }
+
+                Console.WriteLine("Ваша последовательность встречается в массиве " + count + " раз");
+
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine();
+
+            }
 
             //Task 4
             {
